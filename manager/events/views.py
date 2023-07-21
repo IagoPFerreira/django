bdbc2234c2cf8a278da381425342327df9e90acb
Iago <iago.pferreiravr@gmail.com>
2023-07-21 14:59:40 -0300

    feat(Event Manager): Implementando o app "events"

--- manager/events/views.py
+++ manager/events/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from events.models import Event
 
-# Create your views here.
+
+def index(request):
+    context = {"company": "Trybe"}
+    return render(request, 'home.html', context)
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def event_details(request, event_id):
+    try:
+        event = get_object_or_404(Event, id=event_id)
+        return render(request, 'details.html', {'event': event})
+    except Http404:
+        return render(request, '404.html')
